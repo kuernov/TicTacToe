f1709ed40b048e7d27bfc6c2314b8ef4f9a96409
Bartosz <bartosz.malyjasiak@gmail.com>
2023-06-02 23:17:06 +0200

    User set board size and win condition

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -5,113 +5,217 @@
 #define PLAYER2 2
 #define PLAYER1MOVE 'x'
 #define COMPUTERMOVE 'o'
-#define BOARDSIZE 3
+//#define BOARDSIZE 4
+//#define pointsToWin 4
+#define MAX_DEPTH 6
 
 using namespace std;
 
-char board[BOARDSIZE][BOARDSIZE];
+
+
+//char board[BOARDSIZE][BOARDSIZE];
 struct Move {
 	int row, column;
 };
 
-void showBoard(char board[BOARDSIZE][BOARDSIZE])
+
+
+void showBoard(char** board, int size)
 {
-	printf("\n\n");
-
-	printf("\t\t\t  %c | %c  | %c  \n", board[0][0],
-		board[0][1], board[0][2]);
-	printf("\t\t\t--------------\n");
-	printf("\t\t\t  %c | %c  | %c  \n", board[1][0],
-		board[1][1], board[1][2]);
-	printf("\t\t\t--------------\n");
-	printf("\t\t\t  %c | %c  | %c  \n\n", board[2][0],
-		board[2][1], board[2][2]);
-
-	return;
-}
-
-void showInstructions()
-{
-	printf("\t\t\t  Tic-Tac-Toe\n\n");
-	printf("Choose a cell numbered from 1 to 9 as below"
-		" and play\n\n");
-
-	printf("\t\t\t  1 | 2  | 3  \n");
-	printf("\t\t\t--------------\n");
-	printf("\t\t\t  4 | 5  | 6  \n");
-	printf("\t\t\t--------------\n");
-	printf("\t\t\t  7 | 8  | 9  \n\n");
-
-	printf("-\t-\t-\t-\t-\t-\t-\t-\t-\t-\n\n");
-
-	return;
-}
-
-void createBoard() {
-	for (size_t w = 0; w < BOARDSIZE; w++)
+	cout << endl << endl;
+	for (size_t i = 0; i < size; i++)
 	{
-		for (size_t k = 0; k < BOARDSIZE; k++) {
-			board[w][k] = '  ';
+		cout << "\t\t\t  ";
+		for (size_t j = 0; j < size; j++)
+		{
+			cout << board[i][j];
+			if (j < size - 1) {
+				cout << " | ";
+			}
+		}
+		cout << endl;
+		if (i < size - 1) {
+			cout << "\t\t\t";
+			for (size_t k = 0; k < size; k++)
+			{
+				cout << "----";
+			}
+			cout << endl;
+		}
+	}
+	return;
+}
+
+void showInstructions(int size)
+{
+	cout << "\t\t\t  Tic-Tac-Toe" << endl << endl;
+	cout << "Choose a cell numbered from 1 to 9 as below"
+		" and play" << endl << endl;
+	int k = 1;
+	cout << endl << endl;
+	for (size_t i = 0; i < size; i++)
+	{
+		cout << "\t\t\t  ";
+		for (size_t j = 0; j < size; j++)
+		{
+			cout << k++;
+			if (j < size - 1) {
+				cout << " | ";
+			}
+		}
+		cout << endl;
+		if (i < size - 1) {
+			cout << "\t\t\t";
+			for (size_t k = 0; k < size; k++)
+			{
+				cout << "----";
+			}
+			cout << endl;
+		}
+	}
+	return;
+}
+
+void setBoard(char**board, int size) {
+	for (size_t w = 0; w < size; w++)
+	{
+		for (size_t k = 0; k < size; k++) {
+			board[w][k] = ' ';
 		}
 	}
 }
 
 
-bool isGameOver(char player) {
-	for (size_t w = 0; w < BOARDSIZE; w++)
+bool isGameOver(char** board, int size, int pointsToWin, char player) {
+	int streak = 0;
+	for (size_t r = 0; r < size; r++)
 	{
-		if (board[w][0] == player && board[w][1] == player && board[w][2] == player)
+		for (size_t c = 0; c < size; c++)
+		{
+			if (board[r][c] == player)
+				streak++;
+			else
+				streak = 0;
+			if (streak == pointsToWin)
+				return true;
+		}
+		streak = 0;
+	}
+	for (size_t c = 0; c < size; c++)
+	{
+		for (size_t r = 0; r < size; r++) {
+			if (board[r][c] == player)
+				streak++;
+			else
+				streak = 0;
+			if (streak == pointsToWin)
+				return true;
+		}
+		streak = 0;
+	}
+	for (size_t i = 0; i < size; i++)
+	{
+		if (board[i][i] == player)
+			streak++;
+		else
+			streak = 0;
+		if (streak == pointsToWin)
 			return true;
 	}
-	for (size_t k = 0; k < BOARDSIZE; k++)
+	streak = 0;
+	int j = size - 1;
+	for (size_t i = 0; i < size; i++)
 	{
-		if (board[0][k] == player && board[1][k] == player && board[2][k] == player)
+		if (board[i][j] == player)
+			streak++;
+		else
+			streak = 0;
+		if (streak == pointsToWin)
 			return true;
+		j--;
 	}
-	if (board[0][0] == player && board[1][1] == player && board[2][2] == player)
-		return true;
-	if (board[0][2] == player && board[2][2] == player && board[2][0] == player)
-		return true;
 	return false;
 }
 
-int evaluate(char board[BOARDSIZE][BOARDSIZE]) {
-	for (size_t col = 0; col < BOARDSIZE; col++)
+int evaluate(char** board, int size, int pointsToWin, bool isComputer) {
+	int streak = 0;
+	char player;
+	if (isComputer)
+		player = COMPUTERMOVE;
+	else if (!isComputer)
+		player = PLAYER1MOVE;
+	for (size_t r = 0; r < size; r++)
 	{
-		if (board[0][col] == board[1][col] && board[1][col] == board[2][col]) {
-			if (board[0][col] == COMPUTERMOVE)
-				return -10;
-			else if (board[0][col] == PLAYER1MOVE)
-				return 10;
+		for (size_t c = 0; c < size; c++)
+		{
+			if (board[r][c] == player)
+				streak++;
+			else
+				streak = 0;
+			if (streak == pointsToWin) {
+				if (isComputer)
+					return -size * size - 1;
+				else if (!isComputer)
+					return size * size + 1;
+			}
+		}
+		streak = 0;
+	}
+
+	for (size_t c = 0; c < size; c++)
+	{
+		for (size_t r = 0; r < size; r++) {
+			if (board[r][c] == player)
+				streak++;
+			else
+				streak = 0;
+			if (streak == pointsToWin)
+			{
+				if (isComputer)
+					return -size * size - 1;
+				else if (!isComputer)
+					return size * size + 1;
+			}
+		}
+		streak = 0;
+	}
+	for (size_t i = 0; i < size; i++)
+	{
+		if (board[i][i] == player)
+			streak++;
+		else
+			streak = 0;
+		if (streak == pointsToWin)
+		{
+			if (isComputer)
+				return -size * size - 1;
+			else if (!isComputer)
+				return size * size + 1;
 		}
 	}
-	for (size_t r = 0; r < BOARDSIZE; r++)
+	streak = 0;
+	int j = size - 1;
+	for (size_t i = 0; i < size; i++)
 	{
-		if (board[r][0] == board[r][1] && board[r][1] == board[r][2]) {
-			if (board[r][0] == COMPUTERMOVE)
-				return -10;
-			else if (board[r][0] == PLAYER1MOVE)
-				return 10;
+		if (board[i][j] == player)
+			streak++;
+		else
+			streak = 0;
+		if (streak == pointsToWin)
+		{
+			if (isComputer)
+				return -size * size - 1;
+			else if (!isComputer)
+				return size * size + 1;
 		}
-	}
-	if (board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
-		if (board[0][0] == COMPUTERMOVE)
-			return -10;
-		else if (board[0][0] == PLAYER1MOVE)
-			return 10;
-	}
-	if (board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
-		if (board[0][2] == COMPUTERMOVE)
-			return -10;
-		else if (board[0][2] == PLAYER1MOVE)
-			return 10;
+		j--;
 	}
 	return 0;
 }
-bool isBoardFull() {
-	for (size_t w = 0; w < BOARDSIZE; w++)
+bool isBoardFull(char** board, int size) {
+	for (size_t w = 0; w < size; w++)
 	{
-		for (size_t k = 0; k < BOARDSIZE; k++) {
+		for (size_t k = 0; k < size; k++) {
 			if (board[w][k] == ' ')
 				return false;
 		}
@@ -119,29 +223,29 @@ bool isBoardFull() {
 	return true;
 }
 
-int minmax(int depth, bool isMinimazing, int alpha, int beta) {
-	int result = evaluate(board);
+int minmax(char** board, int size,int pointsToWin, int depth, bool isMinimazing, int alpha, int beta) {
+	int result = evaluate(board, size, pointsToWin, !isMinimazing);
 	int best;
-	if (result == -10)
+	if (result == -size*size-1)
 		return result+depth;
-	if (result == 10)
-		return result- depth;
+	if (result == size * size +1)
+		return result-depth;
 
-	if (isBoardFull())
+	if (isBoardFull(board, size) || depth == MAX_DEPTH)
 		return 0;
 	if (isMinimazing) {
-		best =INT_MAX;
-		for (size_t r = 0; r < BOARDSIZE; r++)
+		best = INT_MAX;
+		for (size_t r = 0; r < size; r++)
 		{
-			for (size_t c = 0; c < BOARDSIZE; c++)
+			for (size_t c = 0; c < size; c++)
 			{
 				if (board[r][c] == ' ') {
 					board[r][c] = COMPUTERMOVE;
-					best = min(best, minmax(depth + 1, false, alpha, beta));
+					best = min(best, minmax(board, size, pointsToWin, depth + 1, false, alpha, beta));
 					board[r][c] = ' ';
 					beta = min(beta, best);
 					if (beta <= alpha)
-						break;
+					 	break;
 				}
 			}
 		}
@@ -149,13 +253,13 @@ int minmax(int depth, bool isMinimazing, int alpha, int beta) {
 	}
 	else if (!isMinimazing) {
 		best = INT_MIN;
-		for (size_t r = 0; r < BOARDSIZE; r++)
+		for (size_t r = 0; r < size; r++)
 		{
-			for (size_t c = 0; c < BOARDSIZE; c++)
+			for (size_t c = 0; c < size; c++)
 			{
 				if (board[r][c] == ' ') {
 					board[r][c] = PLAYER1MOVE;
-					best = max(best, minmax(depth + 1, true, alpha, beta));
+					best = max(best, minmax(board, size, pointsToWin, depth + 1, true, alpha, beta));
 					board[r][c] = ' ';
 					alpha = max(alpha, best);
 					if (beta <= alpha)
@@ -165,36 +269,35 @@ int minmax(int depth, bool isMinimazing, int alpha, int beta) {
 		}
 		return best - depth;
 	}
-
-	
+	return 0;
 }
 
 
 
-bool isLegalMove(int row, int column) {
-	if (row < 0 || row>2 || column < 0 || column>2)
+bool isLegalMove(int row, int column, char** board, int size) {
+	if (row < 0 || row>size - 1 || column < 0 || column>size - 1)
 		return false;
 	if (board[row][column] != ' ')
 		return false;
 	return true;
 }
 
-void playerMove(int row, int column, char player) {
+void playerMove(int row, int column, char player, char** board) {
 	board[row][column] = player;
 }
 
-Move findBestMove() {
+Move findBestMove(char** board, int pointsToWin, int size) {
 	int bestValue = INT_MAX;
 	Move bestMove;
 	bestMove.row = -1;
 	bestMove.column = -1;
-	for (size_t r = 0; r < BOARDSIZE; r++)
+	for (size_t r = 0; r < size; r++)
 	{
-		for (size_t c = 0; c < BOARDSIZE; c++)
+		for (size_t c = 0; c < size; c++)
 		{
 			if (board[r][c] == ' ') {
 				board[r][c] = COMPUTERMOVE;
-				int moveValue = minmax(0,false,INT_MIN,INT_MAX);
+				int moveValue = minmax(board, size, pointsToWin, 0, false, INT_MIN, INT_MAX);
 				board[r][c] = ' ';
 				if (moveValue < bestValue) {
 					bestMove.column = c;
@@ -208,8 +311,8 @@ Move findBestMove() {
 	return bestMove;
 }
 
-void playGame(int whoseTurn) {
-	int option= -1;
+void playGame(int whoseTurn, char** board, int size, int pointsToWin) {
+	int option = -1;
 	int move;
 	int row, column;
 	while (option != 0) {
@@ -218,38 +321,39 @@ void playGame(int whoseTurn) {
 		cout << "Choose: ";
 		cin >> option;
 		cout << endl;
-		switch (option){
+		switch (option) {
 		case 1:
-			createBoard();
-			showInstructions();
-			while (!isGameOver(PLAYER1MOVE) && !isGameOver(COMPUTERMOVE) && !isBoardFull()) {
+			setBoard(board,size);
+			showInstructions(size);
+			whoseTurn = PLAYER1;
+			while (!isGameOver(board, size, pointsToWin, PLAYER1MOVE) && !isGameOver(board, size, pointsToWin, COMPUTERMOVE) && !isBoardFull(board, size)) {
 				if (whoseTurn == PLAYER1) {
 					cout << "Wprowadz swoj ruch: ";
 					cin >> move;
 					cout << endl;
-					row = (move - 1) / BOARDSIZE;
-					column = (move - 1) % BOARDSIZE;
-					if (!isLegalMove(row, column)) {
+					row = (move - 1) / size;
+					column = (move - 1) % size;
+					if (!isLegalMove(row, column, board, size)) {
 						cout << "Niepoprawny ruch!" << endl;
 						continue;
 					}
-					playerMove(row, column, PLAYER1MOVE);
-					showBoard(board);
+					playerMove(row, column, PLAYER1MOVE, board);
+					showBoard(board, size);
 					whoseTurn = PLAYER2;
 				}
 				else if (whoseTurn == PLAYER2) {
-					Move move = findBestMove();
-					playerMove(move.row, move.column, COMPUTERMOVE);
-					showBoard(board);
+					Move move = findBestMove(board, size, pointsToWin);
+					playerMove(move.row, move.column, COMPUTERMOVE, board);
+					showBoard(board, size);
 					whoseTurn = PLAYER1;
 				}
 			}
-			if (!isGameOver(PLAYER1MOVE) && !isGameOver(COMPUTERMOVE) && isBoardFull())
+			if (!isGameOver(board, size, pointsToWin, PLAYER1MOVE) && !isGameOver(board, size, pointsToWin, COMPUTERMOVE) && isBoardFull(board, size))
 				cout << "Draw!" << endl;
-			else if (isGameOver(PLAYER1MOVE))
+			else if (isGameOver(board, size, pointsToWin, PLAYER1MOVE))
 				cout << "Player 1 won!" << endl << endl << endl << endl;
-			else if (isGameOver(COMPUTERMOVE))
-				cout << "Computer won!" << endl<<endl<<endl<<endl;
+			else if (isGameOver(board, size, pointsToWin, COMPUTERMOVE))
+				cout << "Computer won!" << endl << endl << endl << endl;
 			break;
 		default:
 			break;
@@ -258,7 +362,47 @@ void playGame(int whoseTurn) {
 	return;
 }
 
+void setGameSettings() {
+
+
+}
+
+void test() {
+	char testBoard[3][3];
+	testBoard[0][0] = 'x';
+	testBoard[0][1] = 'o';
+	testBoard[0][2] = ' ';
+	testBoard[1][0] = 'o';
+	testBoard[1][1] = 'o';
+	testBoard[1][2] = 'x';
+	testBoard[2][0] = 'x';
+	testBoard[2][1] = 'x';
+	testBoard[2][2] = ' ';
+	Move m;
+	//m = findBestMove(testBoard);
+	//cout << m.row << " " << m.column << endl;
+	//showBoard(testBoard);
+	//if (m.row == 2 && m.column == 2)
+	////	return true;
+	//else
+	//	return false;
+}
+
 int main() {
-	playGame(PLAYER1);
+	int size;
+	int streak;
+	cout << "Choose size of the board:  ";
+	cin >> size;
+	cout << endl;
+	cout << "Choose how many points in row gives victory: ";
+	cin >> streak;
+	cout << endl;
+	char** board = new char * [size];
+	for (size_t i = 0; i < size; i++)
+	{
+		board[i] = new char[size];
+	}
+	playGame(PLAYER1,board, size, streak);
+	//cout<<test();
 	return 0;
 }
